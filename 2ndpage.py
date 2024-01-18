6b55f37c11cd0f3f2e61c6d4a4e8df653a82434a
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-19 00:40:18 +0900

    Update 2ndpage.py

--- 2ndpage.py
+++ 2ndpage.py
@@ -12,9 +12,8 @@ import pandas as pd
 uploaded_file = st.file_uploader("CSVファイルを選択してください//(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです。今後最適化を実験するときのために使えるかも)", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-
-if st.button('CSV表示'):
-    st.write(data)
+    if st.button('CSV表示'):
+        st.write(data)
 
 #ここから下は編集しない
 if __name__ == '__main__':
