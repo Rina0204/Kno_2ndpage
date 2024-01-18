fc3c090bbe202fb06d34132aa3a9a01f915321c0
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-19 00:43:12 +0900

    Update 2ndpage.py

--- 2ndpage.py
+++ 2ndpage.py
@@ -9,10 +9,10 @@ st.title('新規作成ページ遷移成功')
 
 #CSVファイルをアップロード(とりあえず)
 import pandas as pd
-uploaded_file = st.file_uploader("CSVファイルを選択してください//(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです。今後最適化を実験するときのために使えるかも)", type="csv")
+uploaded_file = st.file_uploader("CSVファイルを選択してください。(CSVファイルを読み込み表示させられます。今後最適化を実験するときのために使えるかも)", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-    if st.button('CSV表示'):
+    if st.button('CSV表示',use_container_width=True):
         st.write(data)
 
 #ここから下は編集しない
